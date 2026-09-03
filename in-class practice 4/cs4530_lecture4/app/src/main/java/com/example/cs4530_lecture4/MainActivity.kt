a41d0d16030748b7587364eaf5be294e34fe44f9
keatonvoordeckers <keatonvoordeckers@gmail.com>
2026-09-03 13:34:12 -0600

    more reformatting

---
+++ in-class practice 4/cs4530_lecture4/app/src/main/java/com/example/cs4530_lecture4/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.cs4530_lecture4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KeatonApp()
+        }
+    }
+}
+@Composable
+fun KeatonApp() {
+    var text1 by remember { mutableStateOf("") } // First input
+    var text2 by remember { mutableStateOf("") } // Second input
+    var result by remember { mutableStateOf("") } // Combined Result
+
+    // Create Column to store TextFields, Buttons, and the result Text
+    Column{
+        // TextField One
+        OutlinedTextField(
+            value = text1,
+            onValueChange = {text1 = it}
+        )
+        // TextField Two
+        OutlinedTextField(
+            value = text2,
+            onValueChange = {text2 = it}
+
+        )
+    }
+}
