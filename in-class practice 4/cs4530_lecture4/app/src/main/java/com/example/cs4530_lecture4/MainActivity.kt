17d8279534cd08fc1b43bd3694115ac57aa79d2e
keatonvoordeckers <keatonvoordeckers@gmail.com>
2026-09-08 11:00:28 -0600

    Added centering for the UI elements in my Column

--- in-class practice 4/cs4530_lecture4/app/src/main/java/com/example/cs4530_lecture4/MainActivity.kt
+++ in-class practice 4/cs4530_lecture4/app/src/main/java/com/example/cs4530_lecture4/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -13,6 +15,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat.enableEdgeToEdge
 
 
@@ -32,7 +36,11 @@ fun KeatonApp() {
     var result by remember { mutableStateOf("") } // Combined Result
 
     // Create Column to store TextFields, Buttons, and the result Text
-    Column{
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
         // TextField One
         OutlinedTextField(
             value = text1,
